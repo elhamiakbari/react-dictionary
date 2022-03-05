8ee4d84ecedb713d1b7e35ffa6046582c1c812b6
elhamiakbari <93420421+elhamiakbari@users.noreply.github.com>
2022-03-05 13:26:54 +0330

    upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.png'
+import github from './github.png'
+import Dictionary from './Dictionary'
+import Result from './Result'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="Container">
+        <header className="App-header">
+            <img src={logo} className="logo" alt="logo" />
+        </header>
+        <Dictionary defaultKeyword="amazing" />
+        <Result defaultKeyword="amazing" />
+        <footer>
+          coded by<em> Elham Akbari</em> , open source code on{' '}
+          <a
+            href="https://github.com/elhamakbariii/react-dictionary-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+             <strong>github</strong>
+          </a>
+        </footer>
+      </div>
+    </div>
+  )
+}
